b96f2e316c06026bebadbe6da429eb1151822ba3
etgreer91 <etgreer91@gmail.com>
2020-06-12 14:06:26 -1000

    addresses bugs and connects static components into application

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SearchBar.css';
 
-sortByOptions = {
+const sortByOptions = {
     'Best Match': 'best_match',
     'Highest Rated': 'rating',
     'Most Reviewed': 'review_count'
@@ -9,27 +9,30 @@ sortByOptions = {
 
 class SearchBar extends React.Component {
     renderSortByOptions() {
-        return Object.keys(SortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortbyOptions[sortByOption];
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
             return <li key={sortByOptionValue}>{sortByOption}</li>;
         })
     }
 
     render() {
-        <div className="SearchBar">
-           <div className="SearchBar-sort-options">
-              <ul>
-                 {this.renderSortByOptions}
-              </ul>
-           </div>
-           <div className="SearchBar-fields">
-              <input placeholder="Search Businesses" />
-              <input placeholder="Where?" />
-           </div>
-           <div className="SearchBar-submit">
-              <a>Let's Go</a>
-           </div>
-        </div> 
+        return(
+            <div className="SearchBar">
+               <div className="SearchBar-sort-options">
+                   <ul>
+                      {this.renderSortByOptions}
+                   </ul>
+               </div>
+               <div className="SearchBar-fields">
+                   <input placeholder="Search Businesses" />
+                   <input placeholder="Where?" />
+               </div>
+            <div className="SearchBar-submit">
+               <a>Let's Go</a>
+            </div>
+        </div>
+
+        )
     }
 }
 
